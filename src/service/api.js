7fc84227a5aeec4396efdaecf1303615ec84d0b5
DanielMelloFarias <danielmellofarias@gmail.com>
2025-04-13 21:49:36 -0300

    update api.js - ajustei pra puxar os dados

--- src/service/api.js
+++ src/service/api.js
@@ -1,15 +1,16 @@
 // src/services/api.js
 const ACCESS_TOKEN = '826de2f3234e1aeccc645180ada45d3aacdd73a1';
 
+const baseUrl = process.env.NODE_ENV === 'development'
+  ? '' // Usa o proxy em desenvolvimento
+  : 'https://monitoramento.wylken.com.br'; // URL absoluta em produção
+
 export const fetchRegistros = async () => {
   try {
-    // Caminho relativo - o proxy se encarrega de redirecionar
-    const response = await fetch(`/api/registros?access_token=${ACCESS_TOKEN}`);
-    
+    const response = await fetch(`${baseUrl}/api/registros?access_token=${ACCESS_TOKEN}`);
     if (!response.ok) {
       throw new Error(`Erro: ${response.status} ${response.statusText}`);
     }
-    
     return await response.json();
   } catch (error) {
     console.error('Erro ao buscar registros:', error);
